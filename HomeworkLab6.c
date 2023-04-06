b1b5b5ee3dc0144949f1ee73ccfc9295762d55ed
Laoo02 <laurentiutaran72@gmail.com>
2023-04-06 10:04:23 +0300

    started working on directory +fixed switch issue

--- HomeworkLab6.c
+++ HomeworkLab6.c
@@ -1,106 +1,131 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 
+/* void FileType(char var,char filename[50],char option[10])
+ {
+     if(var =='d')
+     {
+         directory(filename,option);
+     }
+ }
 
-          void menu(char filename[50],char option[10]){
-         printf("--MENU--\n");
-        printf("Options:\n");
-         printf("name:n\n");
-          printf("last modified:m\n");
-           printf("a:access\n");
-           printf("***********************************************************************\n");
-           options(filename,option);
-    }
 
-    void options(char filename[50],char option[10])
+ void directory(char filename[50],char option[10])
+ {
+     if(DIR *opendir(filename) ==NULL)
+     {
+         printf("Could not open the %s directory!\n",filename);
+         exit(1);
+     }
+
+     struct dirent *readdir(DIR *dirp);
+
+ }*/
+
+void menu(char filename[50], char option[10])
+{
+    printf("--MENU--\n");
+    printf("Options:\n");
+    printf("name:n\n");
+    printf("last modified:m\n");
+    printf("a:access\n");
+    printf("***********************************************************************\n");
+    options(filename, option);
+}
+
+void options(char filename[50], char option[10])
+{
+    struct stat *buf;
+    buf = malloc(sizeof(struct stat));
+
+    if (stat(filename, buf) < 0)
     {
-        struct stat *buf;
-        buf=malloc(sizeof(struct stat));
-
-                if(stat(filename,buf)<0)
-                {
-                    printf("Error!Cannot read file permissions!\n");
-                    exit(1);
-                }
-
-        for(int i=1;i<strlen(option);i++)
-        {
-            switch(option[i])
-            {
-                case 'n':
-                printf("The name is:%s\n",filename);
-                break;
-
-                case 'a':
-              permissions(buf->st_mode);
-                break;
-
-                case 'm':
-                //last_modified(buf->st_mtime);
-                printf("Last time the file was modified:%s",ctime(&buf->st_mtime));
-                break;
-
-                case 'h':
-                printf("The file named %s has %d hard links",filename,buf->st_nlink);
-                break;
-
-                case 'l':
-                create_SL(filename);
-
-                default:
-                printf("invalid input!\n");
-            }   
-            printf("***********************************************************************\n");
-        }
+        printf("Error!Cannot read file permissions!\n");
+        exit(1);
     }
 
-    void permissions(unsigned short mode)
+    for (int i = 1; i < strlen(option); i++)
     {
-        printf("Access rights: \n");
-              printf("USER:\n");
-              printf( (mode & S_IRUSR) ? "Read:yes\n" : "Read:no\n");
-              printf( (mode & S_IWUSR) ? "Write:yes\n" : "Write:no\n");
-              printf( (mode & S_IXUSR) ? "Execution:yes\n" : "Execution:no\n");
-              printf("GROUP:\n");
-              printf( (mode & S_IRGRP) ? "Read:yes\n" : "Read:no\n");
-              printf( (mode & S_IWGRP) ? "Write:yes\n" : "Write:no\n");
-              printf( (mode & S_IXGRP) ? "Execution:yes\n" : "Execution:no\n");
-              printf("OTHER:");
-              printf( (mode & S_IROTH) ? "Read:yes\n" : "Read:no\n");
-              printf( (mode & S_IWOTH) ? "Write:yes\n" : "Write:no\n");
-              printf( (mode & S_IXOTH) ? "Execution:yes\n" : "Execution:no\n");
-    }
+        switch (option[i])
+        {
+        case 'n':
+            printf("The name is:%s\n", filename);
+            break;
 
-    void create_SL(char filename[50])
+        case 'a':
+            permissions(buf->st_mode);
+            break;
+
+        case 'm':
+            // last_modified(buf->st_mtime);
+            printf("Last time the file was modified:%s", ctime(&buf->st_mtime));
+            break;
+
+        case 'h':
+            printf("The file named %s has %d hard links", filename, buf->st_nlink);
+            break;
+
+        case 'l':
+            create_SL(filename);
+            break;
+
+        default:
+            printf("invalid input!\n");
+            break;
+        }
+        printf("***********************************************************************\n");
+    }
+}
+
+void permissions(unsigned short mode)
+{
+    printf("Access rights: \n");
+    printf("USER:\n");
+    printf((mode & S_IRUSR) ? "Read:yes\n" : "Read:no\n");
+    printf((mode & S_IWUSR) ? "Write:yes\n" : "Write:no\n");
+    printf((mode & S_IXUSR) ? "Execution:yes\n" : "Execution:no\n");
+    printf("GROUP:\n");
+    printf((mode & S_IRGRP) ? "Read:yes\n" : "Read:no\n");
+    printf((mode & S_IWGRP) ? "Write:yes\n" : "Write:no\n");
+    printf((mode & S_IXGRP) ? "Execution:yes\n" : "Execution:no\n");
+    printf("OTHER:");
+    printf((mode & S_IROTH) ? "Read:yes\n" : "Read:no\n");
+    printf((mode & S_IWOTH) ? "Write:yes\n" : "Write:no\n");
+    printf((mode & S_IXOTH) ? "Execution:yes\n" : "Execution:no\n");
+}
+
+void create_SL(char filename[50])
+{
+    char SL_name[50];
+    printf("Choose a softlink file name:");
+    scanf("%s", &SL_name);
+    printf("\n");
+    printf("file name:%s\n", SL_name);
+    int Sl_test = symlink(filename, SL_name);
+    if (Sl_test == 0)
     {
-        char SL_name[50];
-        printf("Choose a softlink file name:");
-        scanf("%s",&SL_name);
-       /* printf("\n");
-        printf("file name:%s\n",SL_name);*/
-        //int Sl_test=symlink(filename,SL_name);
-        if(Sl_test == 0)
-        {
-            printf("Soft link added!");
-        }
-
+        printf("Soft link added!");
     }
+}
 
-
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
     {
-        if(argc < 3 )
-        {
-            printf("Too few arguments provided!\n");
-        }
-        else
-        {
-            menu(argv[1],argv[2]);
-        }
-     
+        printf("Too few arguments provided!\n");
     }
+    /* else if(argc ==4)
+     {
+         FileType(argv[4]);
+     }*/
+    else
+    {
+        menu(argv[1], argv[2]);
+        // options(argv[1],argv[2]);
+    }
+}
